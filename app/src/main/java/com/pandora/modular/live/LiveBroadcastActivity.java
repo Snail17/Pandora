451eefc89c692196a6f3f700da5f90bc34f5de7a
Feng <droidfeng@163.com>
2018-05-10 19:23:11 +0800

    添加直播界面UI

---
+++ app/src/main/java/com/pandora/modular/live/LiveBroadcastActivity.java
@@ -0,0 +1,15 @@
+package com.pandora.modular.live;
+
+import android.os.Bundle;
+
+import com.pandora.R;
+import com.pandora.core.base.BaseActivity;
+
+public class LiveBroadcastActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_broadcast);
+    }
+}
--- app/src/main/java/com/pandora/modular/main/fragment/HomeFragment.java
+++ app/src/main/java/com/pandora/modular/main/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.pandora.modular.main.fragment;
 
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
