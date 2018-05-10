014198ed01f5f979c2b0d9466fd762ce611e3e21
Snail1995 <fenghongjing2016@163.com>
2018-05-10 23:28:02 +0800

    闪屏页

--- app/src/main/java/com/pandora/modular/splash/activity/SplashActivity.java
+++ app/src/main/java/com/pandora/modular/splash/activity/SplashActivity.java
@@ -1,16 +1,78 @@
 package com.pandora.modular.splash.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.pandora.R;
 import com.pandora.core.base.BaseActivity;
+import com.pandora.modular.main.activity.MainActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SplashActivity extends BaseActivity {
+
+    public static final int SECOND = 3;
+    private int secondCount;
+    // 该方法必须不能为private或者是static的
+    @BindView(R.id.tv_splash_time)
+    TextView tvSplashTime;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    if (msg.arg1 != 0) {
+                        tvSplashTime.setText(msg.arg1 + "s");
+                    } else {
+                        goToMain();
+                    }
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        secondCount = SECOND;
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Message message = Message.obtain();
+                message.what = 1;
+                message.arg1 = secondCount--;
+                mHandler.handleMessage(message);
+                mHandler.postDelayed(this, 1000);
+            }
+        };
+        mHandler.post(r);
+    }
 
+    @OnClick(R.id.tv_splash_time)
+    public void onclick(View view) {
+        goToMain();
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
