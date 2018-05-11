3d2e3a609a3134a1b0a78b2ccd3ed6821ed92fab
Feng <droidfeng@163.com>
2018-05-12 03:35:43 +0800

    添加首页轮播图

--- app/src/main/java/com/pandora/modular/home/fragment/HomeFragment.java
+++ app/src/main/java/com/pandora/modular/home/fragment/HomeFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.pandora.R;
@@ -23,6 +25,7 @@ import com.pandora.modular.home.prenster.DaggerHomeComponent;
 import com.pandora.modular.home.prenster.HomeContract;
 import com.pandora.modular.home.prenster.HomeModule;
 import com.pandora.modular.home.prenster.HomePresenter;
+import com.pandora.modular.home.widght.RecyclerBanner;
 import com.pandora.modular.live.activity.LiveBroadcastActivity;
 
 import java.util.ArrayList;
@@ -42,11 +45,12 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
     @BindView(R.id.tv_home_top_introduce)
     TextView introduceText;
 
-    @BindView(R.id.pager_home_top)
-    ViewPager mViewPager;
 
     @BindView(R.id.recycler_home_view)
     RecyclerView mRecyclerView;
+
+    @BindView(R.id.banner_home)
+    RecyclerBanner mBanner;
 
     @Inject
     HomePresenter mHomePresenter;
@@ -54,6 +58,7 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
 
     private HomeBean mHomeBean;
     private List<HomeBean.HomeData> mHomeData;
+    private List<RecyclerBanner.BannerEntity> urls = new ArrayList<>();
 
     public HomeFragment() {
     }
@@ -89,6 +94,16 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
         });
         mRecyclerView.setAdapter(mAdapter);
         mHomePresenter.getData();
+        initBanner();
+    }
+
+    private void initBanner() {
+        mBanner.setOnPagerClickListener(new RecyclerBanner.OnPagerClickListener() {
+            @Override
+            public void onClick(RecyclerBanner.BannerEntity entity) {
+                Toast.makeText(HomeFragment.this.getContext(), entity.getUrl(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -97,12 +112,36 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
         introduceText.setSelected(true);
     }
 
+
+    public void update() {
+        urls.add(new Entity("http://pic.58pic.com/58pic/12/46/13/03B58PICXxE.jpg"));
+        urls.add(new Entity("http://www.jitu5.com/uploads/allimg/121120/260529-121120232T546.jpg"));
+        urls.add(new Entity("http://pic34.nipic.com/20131025/2531170_132447503000_2.jpg"));
+        urls.add(new Entity("http://img5.imgtn.bdimg.com/it/u=3462610901,3870573928&fm=206&gp=0.jpg"));
+        mBanner.setDatas(urls);
+    }
+
+    private class Entity implements RecyclerBanner.BannerEntity {
+
+        String url;
+
+        public Entity(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public String getUrl() {
+            return url;
+        }
+    }
+
     @Override
     public void setData(HomeBean data) {
         mHomeBean = data;
         introduceText.setText(mHomeBean.getOnlineService());
         mHomeData.addAll(data.getData());
         mAdapter.notifyDataSetChanged();
+        update();
     }
 
 
